eb45f9d83ab3cb4eb380a346e7a84cacd4e24547
jiyan-0525 <jiyanw825@gmail.com>
2026-06-27 09:47:31 +0000

    mofid

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,11 +2,5 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: ./bitcoinExchange <filename>" << std::endl;
-        return 1;
-    }
-    BitcoinExchange exchange;
-    exchange.run(argv[1]);
-    return 0;
+ 
 }
