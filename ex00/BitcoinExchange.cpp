e821fadfb70ddf0e381fbe68c66f9222a279b05f
jiyan-0525 <jiyanw825@gmail.com>
2026-06-27 09:43:24 +0000

    update

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,1 +1,57 @@
-#include ""
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange() {}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {
+    *this = other;
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
+    if (this != &other) {
+        data = other.data;
+    }
+    return *this;
+}
+
+void BitcoinExchange::run(const std::string& filename) {
+    loadData();
+    processInput(filename);
+}
+
+void BitcoinExchange::loadData() {
+    std::ifstream file("data.csv");
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line)) {
+        processLine(line);
+    }
+}
+
+void BitcoinExchange::processLine(const std::string& line) {
+    std::string date;
+    double value;
+    std::stringstream ss(line);
+    std::getline(ss, date, ',');
+    ss >> value;
+    data[date] = value;
+}
+
+void BitcoinExchange::processInput(const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line)) {
+        processData(line);
+    }
+}
+
+void BitcoinExchange::processData(const std::string& line) {
+    std::string date;
+    double value;
+    std::stringstream ss(line);
+    std::getline(ss, date, ',');
+    ss >> value;
+    data[date] = value;
+}
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include "BitcoinExchange.hpp"
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./bitcoinExchange <filename>" << std::endl;
+        return 1;
+    }
+    BitcoinExchange exchange;
+    exchange.run(argv[1]);
+    return 0;
 }
