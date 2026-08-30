cbd6f864c9ee937d32377b07e9e7d7d14d019ab1
ThePrimeagen <the.primeagen@aol.com>
2026-08-30 12:10:35 +0200

    add

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,5 +1,4 @@
 #include "PmergeMe.hpp"
-#include <cstdlib>
 
 PmergeMe::PmergeMe() {}
 
@@ -15,37 +14,3 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
 
 PmergeMe::~PmergeMe() {}
 
-void PmergeMe::fillContainers(int argc, char **argv) {
-    for (int i = 1; i < argc; ++i) {
-        const char *s = argv[i];
-        while (*s) {
-            if (!std::isdigit(*s)) {
-                std::cerr << "Error" << std::endl;
-                exit(1);
-            }
-            ++s;
-        }
-        long v = std::atol(argv[i]);
-        if (v < 0) {
-            std::cerr << "Error" << std::endl;
-            exit(1);
-        }
-        int vi = static_cast<int>(v);
-        _vector.push_back(vi);
-        _deque.push_back(vi);
-    }
-}
-
-void PmergeMe::printContainers() const {
-    for (std::vector<int>::const_iterator it = _vector.begin(); it != _vector.end(); ++it)
-        std::cout << " " << *it;
-    std::cout << std::endl;
-}
-
-std::vector<int>& PmergeMe::vector() {
-    return _vector;
-}
-
-std::deque<int>& PmergeMe::deque() {
-    return _deque;
-}
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -3,6 +3,7 @@
 #include <deque>
 #include <vector>
 #include <cctype>
+#include <cstdlib>
 #include <utility>
 #include <iostream>
 #include <algorithm>
@@ -14,79 +15,16 @@ public:
     PmergeMe& operator=(const PmergeMe& other);
     ~PmergeMe();
 
-    void fillContainers(int argc, char **argv);
-    void printContainers() const;
-
-    std::vector<int>& vector();
-    std::deque<int>& deque();
+    void sortVector();
+    void sortDeque();
 
 private:
     std::vector<int> _vector;
     std::deque<int> _deque;
+
+    static std::vector<int> mergeinsertVector(const std::vector<int>& vec);
+    static std::deque<int> mergeinsertDeque(const std::deque<int>& deq);
+    static std::vector<size_t> jacobsthalSequence(size_t n);
 };
-
-template <typename T>
-void mergeInsertionSort(T& container) {
-    if (container.size() <= 1)
-        return;
-
-    typedef typename T::value_type value_type;
-
-    std::vector<std::pair<value_type, value_type> > pairs;
-    bool hasOdd = false;
-    value_type odd;
-    typename T::iterator it = container.begin();
-    while (it != container.end()) {
-        value_type a = *it;
-        ++it;
-        if (it == container.end()) {
-            hasOdd = true;
-            odd = a;
-            break;
-        }
-        value_type b = *it;
-        ++it;
-        if (a > b)
-            std::swap(a, b);
-        pairs.push_back(std::make_pair(a, b));
-    }
-
-    T main;
-    std::vector<value_type> pend;
-    for (typename std::vector<std::pair<value_type, value_type> >::iterator p = pairs.begin();
-         p != pairs.end(); ++p)
-    {
-        main.push_back(p->second);
-        pend.push_back(p->first);
-    }
-    if (main.size() > 1)
-        mergeInsertionSort(main);
-
-    T sorted(main);
-
-    std::vector<std::size_t> order;
-
-    // Jacobsthal insertion
-    for (std::size_t idx = 2; idx < pend.size() || idx - 1 < pend.size(); idx += 2) {
-        if (idx < pend.size())
-            order.push_back(idx);
-        if (idx - 1 < pend.size())
-            order.push_back(idx - 1);
-    }
-    for (std::size_t k = 0; k < order.size(); ++k) {
-        std::size_t i = order[k];
-        typename T::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), pend[i]);
-        sorted.insert(pos, pend[i]);
-    }
-    {
-        typename T::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), pend[0]);
-        sorted.insert(pos, pend[0]);
-    }
-    if (hasOdd) {
-        typename T::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), odd);
-        sorted.insert(pos, odd);
-    }
-    container.assign(sorted.begin(), sorted.end());
-}
 
 #endif
